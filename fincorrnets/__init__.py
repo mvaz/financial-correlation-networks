fa1d41059c3872c5d6fda7952a9fd6215aae2d30
Miguel Vaz <migueljvaz@gmail.com>
2016-04-12 23:43:05 +0100

    added viz to __init__.py

--- fincorrnets/__init__.py
+++ fincorrnets/__init__.py
@@ -1,2 +1,3 @@
 from .algos import construct_mst, construct_pmfg, compute_distance, compute_centrality_metrics
+from .viz import nodes_datasource, edges_datasource, plot_edges, plot_nodes, plot_graph
 
